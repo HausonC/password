c325dc11921a33138f1803a5f9961efdaecab225
Hauson Chen <hauson621@hotmail.com.tw>
2024-09-11 15:08:14 +0800

    final ver

--- password/pw12345.py
+++ password/pw12345.py
@@ -1,13 +1,16 @@
 password = 'a12345'
 y = 3 # input times limitation
-# while True
+# while True:
 while y > 0:
+	y = y - 1
 	pwd = input ('please input password: ')
 	if pwd == password:
 		print ('log in sucessfully')
 		break  #jump out loop
 	else:
-		y = y - 1
-		print ('pw is incorrect! you still have', y,'chance')
+		if y > 0:
+			print ('pw is incorrect! you still have', y,'chance')
+		else:
+			print ('your account is locked, please contact IT')
 		# if y == 0:	//if use while True
 		#	break		//if use while True
